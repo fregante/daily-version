6107109febaa9bd3674776e7734b739b2452f86e
Federico Brigante <github@bfred.it>
2017-08-29 15:13:23 +0700

    Change cache directory name

--- index.sh
+++ index.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-mkdir -p ~/.version-store/
+mkdir -p ~/.npm-daily-version/
 
 CURR=$(date -u +%y.%-m.%-d)
-PREV=$(cat ~/.version-store/v 2>/dev/null || true)
+PREV=$(cat ~/.npm-daily-version/v 2>/dev/null || true)
 
 if [[ $PREV =~ ^$CURR ]]; then
 	CURR=$(date -u +%y.%-m.%-d.%-H%M)
 fi
 
-echo "$CURR" > ~/.version-store/v
+echo "$CURR" > ~/.npm-daily-version/v
 echo "$CURR"
