df44779d994a9f28518da9439ef08aa759ca8e10
Federico Brigante <github@bfred.it>
2017-08-29 18:54:56 +0700

    Add year format option

--- index.sh
+++ index.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 mkdir -p ~/.npm-daily-version/
-
-CURR=$(date -u +%y.%-m.%-d)
+YEAR=${1:-y}
+CURR=$(date -u +%"$YEAR".%-m.%-d)
 PREV=$(cat ~/.npm-daily-version/v 2>/dev/null || true)
 
 if [[ $PREV =~ ^$CURR ]]; then
-	CURR=$(date -u +%y.%-m.%-d.%-H%M)
+	CURR=$(date -u +%"$YEAR".%-m.%-d.%-H%M)
 fi
 
 echo "$CURR" > ~/.npm-daily-version/v
