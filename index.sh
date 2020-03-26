b9775e2d6efa5e4b7073f021d37d61d23db4a90c
Federico Brigante <opensource@bfred.it>
2020-03-26 19:11:14 +0100

    Add support for prefixes and restore support for full years (#1)

--- index.sh
+++ index.sh
@@ -1,12 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
-YEAR=${1:-y}
-CURR=$(date -u +%"$YEAR".%-m.%-d)
+PREFIX=$1
+TODAY=$(npx utc-version --short)
 
 # Check if there's already a tag for today
-if [ "$(git tag -l "$CURR")" ]; then
-	utc-version
+if [ "$(git tag -l "$PREFIX$TODAY")" ]; then
+	echo "$PREFIX$(npx utc-version)"
 else
-	echo "$CURR"
+	echo "$PREFIX$TODAY"
 fi
-
